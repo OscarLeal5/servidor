d0367d6b525e37ba112e2b9208b1c74a24586bb3
oscarleal5 <oscar.leal@cetys.edu.mx>
2021-09-22 12:28:50 -0700

    heroku settings

--- innovativenet/innovativenet/innovativenet/settings.py
+++ innovativenet/innovativenet/innovativenet/settings.py
@@ -133,6 +133,7 @@ LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

542f2ba769d64813536825134e1763216a105ad5
oscarleal <oscar.leal@cetys.edu.mx>
2021-10-05 15:23:35 -0700

    cambios en general

--- innovativenet/innovativenet/innovativenet/settings.py
+++ innovativenet/innovativenet/innovativenet/settings.py
@@ -143,7 +143,7 @@ LOGIN_URL = 'login'
 
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type

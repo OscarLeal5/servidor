76721f9e2d492b62f1df09926bbcf3110b345a81
oscarleal <oscar.leal@cetys.edu.mx>
2021-11-05 12:22:11 -0700

    modificar tabla mmtos y views.py

--- innovativenet/innovativenet/RPA/views.py
+++ innovativenet/innovativenet/RPA/views.py
@@ -162,11 +162,11 @@ class Agregar_Mantenimiento(LoginRequiredMixin, CreateView):
 
 class MttoUpdate(LoginRequiredMixin, UpdateView):
     model = Mantenimiento
-    fields = ['Titulo','periodisidadactividades', 'periodisidadadicional']
+    fields = ['titulonombre','periodisidadactividades', 'periodisidadadicional','cantidaddedispositivos', 'cantidaddispositivosextras',]
     template_name = 'mantenimientos/modificar_servicio.html'
 
     def get_success_url(self):
-        return reverse('detalle_cotizacion', kwargs={'cotizacion':self.object.cotizacion.id})
+        return reverse('detalle_cotizacion', kwargs={'pk':self.object.cotizacion.id,'cliente':self.object.cotizacion.cliente.id})
 
 
 
@@ -176,7 +176,7 @@ class EliminarMantenimiento(LoginRequiredMixin, DeleteView):
     template_name = 'mantenimientos/eliminar_servicio.html'
     
     def get_success_url(self):
-        return reverse('detalle_cotizacion', kwargs={'cotizacion':self.object.cotizacion.id})
+        return reverse('detalle_cotizacion', kwargs={'pk':self.object.cotizacion.id,'cliente':self.object.cotizacion.cliente.id})
 
 
 class Detalle_Servicio(LoginRequiredMixin, DetailView):
